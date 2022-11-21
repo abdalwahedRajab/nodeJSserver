f469e89b2ccbc93e7c4140596aa8b7e8e31d6900
abdalwahedRajab <116273280+abdalwahedRajab@users.noreply.github.com>
2022-11-21 22:52:10 +0300

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -14,7 +14,9 @@ const signToken = id => {
 const creatSendToken = (user,statusCode,res)=>{
     const token = signToken(user._id)
     const cookieOptions = {
-        expires:new Date(Date.now()+ process.env.JWT_COKIE_EXPIRES_IN * 24 *60 *60 *1000)}
+        expires:new Date(Date.now()+ process.env.JWT_COKIE_EXPIRES_IN * 24 *60 *60 *1000),
+        httpOnly: true
+    }
     if (process.env.NODE_ENV==='production') cookieOptions.secure = true;
 
     res.cookie('jwt',token,cookieOptions)
@@ -54,7 +56,8 @@ exports.login = catchAsync(async(req,res,next)=>{
 
 exports.logout = catchAsync(async(req,res,next)=>{
     res.cookie('jwt','loogedout',{
-        expires:new Date ( Date.now() + 1 * 1000)
+        expires:new Date ( Date.now() + 1 * 1000),
+        httpOnly:true
     })
     res.status(200).json({
         status:'success'
