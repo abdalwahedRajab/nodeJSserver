f1ee1dbd6f4b1409c274d597278e068752781683
abdalwahedRajab <116273280+abdalwahedRajab@users.noreply.github.com>
2022-11-22 14:16:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,31 @@ const userRoute=require('./route/userRoute')
 const app = express();
 
 app.use(cors({
-    origin:"http://ja-vognmand.dk",
+    origin:'http://ja-vognmand.dk',
     credentials:true
 
 })) 
 app.use(express.json({limit:'10kb'}));
 app.use(cookieParser())
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://ja-vognmand.dk');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 //route
 app.use('/api/v1/users',userRoute);
