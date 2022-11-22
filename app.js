b82d6377ad7669a857a8d5b567920b8dde18577f
abdalwahedRajab <116273280+abdalwahedRajab@users.noreply.github.com>
2022-11-22 16:15:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,22 +19,10 @@ app.options("*",cors())
 app.use(express.json({limit:'10kb'}));
 app.use(cookieParser())
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://185.20.205.59');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,     Content-Type");
     next();
 });
 
