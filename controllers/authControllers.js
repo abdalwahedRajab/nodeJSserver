5741ece61074450648a40ead67b186f9f3764a76
abdalwahedRajab <116273280+abdalwahedRajab@users.noreply.github.com>
2022-11-23 01:44:47 +0300

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -16,6 +16,7 @@ const creatSendToken = (user,statusCode,res)=>{
     const cookieOptions = {
         expires:new Date(Date.now()+ process.env.JWT_COKIE_EXPIRES_IN * 24 *60 *60 *1000),
         httpOnly: true,
+        sameSite:"none",
         domain:"ja-vognamnddd.adaptable.app"
     }
     if (process.env.NODE_ENV==='production') cookieOptions.secure = true;
@@ -60,6 +61,7 @@ exports.logout = catchAsync(async(req,res,next)=>{
     res.cookie('jwt','loogedout',{
         expires:new Date ( Date.now() + 1 * 1000),
         httpOnly:true,
+        sameSite:"none",
         domain:"ja-vognamnddd.adaptable.app"
     })
     res.status(200).json({
